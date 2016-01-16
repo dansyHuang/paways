b2dfac8ccd025ce399b8ea3b5b27f5814a2e6bf5
DansyHuang <419494396@qq.com>
2016-01-16 21:54:29 +0800

    delete

--- quickstart/src/main/java/org/pways/website/repository/PawaysRoleDao.java
+++
@@ -1,7 +0,0 @@
-package org.pways.website.repository;
-
-import org.pways.website.entity.PawaysRole;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface PawaysRoleDao extends PagingAndSortingRepository<PawaysRole, Long> {
-}
